f6521fb2171da17779f0a75ceebbd82641841f68
Dominik Sidorczuk <domik141202@gmail.com>
2024-10-27 00:00:31 +0200

    add function to get probably visible tiles from any position

--- dominik.py
+++ dominik.py
@@ -9,19 +9,25 @@ class Pos:
     y: int
 
 
+@dataclass
+class ItemData:
+    type: ItemType
+    tick: int
+
+
 class MyBot(HackathonBot):
     def __init__(self) -> None:
         super().__init__()
         self.pos = None
-        self.notwallmap: list[list[int]] = []
+        self.wallmap: list[list[int]] = []
         self.init: bool = False
         self.direction: Direction = None
         self.turretDirection: Direction = None
         self.dimension = None
-        Tile
+        self.knownItems: dict[Pos|ItemData] = {}
 
     def analize_map(self, map: Map):
-        self.notwallmap = [[False for _ in range(self.dimension)] for _ in range(self.dimension)]
+        self.wallmap = [[False for _ in range(self.dimension)] for _ in range(self.dimension)]
         for y, line in enumerate(map.tiles):
             for x, tile in enumerate(line):
                 for entity in tile.entities:
@@ -30,78 +36,81 @@ class MyBot(HackathonBot):
                         self.direction = entity.direction
                         self.turretDirection =  entity.turret.direction
                     elif isinstance(entity, Wall):
-                        self.notwallmap[y][x] = True
+                        self.wallmap[y][x] = True
         self.init = True
 
-    def get_tiles_to_see(self):
+    def get_tiles_probably_visible(self, pos: Pos, direction: Direction, turretDirection: Direction) -> list[Pos]:
         tiles_to_see = []
-        if self.turretDirection != self.direction:
-            if self.turretDirection == Direction.UP:
-                y = self.pos.y - 1
+        if turretDirection != direction:
+            if turretDirection == Direction.UP:
+                y = pos.y - 1
                 while y >= 0:
-                    if self.notwallmap[y][self.pos.x]:
+                    if self.wallmap[y][pos.x]:
                         break
-                    tiles_to_see.append(Pos(self.pos.x, y))
+                    tiles_to_see.append(Pos(pos.x, y))
                     y -= 1
-            elif self.turretDirection == Direction.RIGHT:
-                x = self.pos.x + 1
+            elif turretDirection == Direction.RIGHT:
+                x = pos.x + 1
                 while x < self.dimension:
-                    if self.notwallmap[self.pos.y][x]:
+                    if self.wallmap[pos.y][x]:
                         break
-                    tiles_to_see.append(Pos(x, self.pos.y))
+                    tiles_to_see.append(Pos(x, pos.y))
                     x += 1
-            elif self.turretDirection == Direction.DOWN:
-                y = self.pos.y + 1
+            elif turretDirection == Direction.DOWN:
+                y = pos.y + 1
                 while y < self.dimension:
-                    if self.notwallmap[y][self.pos.x]:
+                    if self.wallmap[y][self.pos.x]:
                         break
                     tiles_to_see.append(Pos(self.pos.x, y))
                     y += 1
-            elif self.turretDirection == Direction.LEFT:
-                x = self.pos.x - 1
+            elif turretDirection == Direction.LEFT:
+                x = pos.x - 1
                 while x >= 0:
-                    if self.notwallmap[self.pos.y][x]:
+                    if self.wallmap[pos.y][x]:
                         break
-                    tiles_to_see.append(Pos(x, self.pos.y))
+                    tiles_to_see.append(Pos(x, pos.y))
                     x -= 1
         a = math.tan(math.pi/10)
-        if self.direction == Direction.UP:
+        if direction == Direction.UP:
             for x in range(self.dimension):
-                if x == self.pos.x:
+                if x == pos.x:
                     a = -a
-                y = int(round((x - self.pos.x) * a + self.pos.y, 0))
+                y = int(round((x - pos.x) * a + pos.y, 0))
                 while y >= 0:
-                    if not self.notwallmap[y][x]:
+                    if not self.wallmap[y][x]:
                         tiles_to_see.append(Pos(x, y))
                     y -= 1
-        elif self.direction == Direction.DOWN:
+        elif direction == Direction.DOWN:
             for x in range(self.dimension):
-                if x == self.pos.x:
+                if x == pos.x:
                     a = -a
-                y = int(round((self.pos.x - x) * a + self.pos.y, 0))
+                y = int(round((pos.x - x) * a + pos.y, 0))
                 while y < self.dimension:
-                    if not self.notwallmap[y][x]:
+                    if not self.wallmap[y][x]:
                         tiles_to_see.append(Pos(x, y))
                     y += 1
-        elif self.direction == Direction.RIGHT:
+        elif direction == Direction.RIGHT:
             for y in range(self.dimension):
-                if y == self.pos.y:
+                if y == pos.y:
                     a = -a
-                x = int(round(self.pos.x + a*(self.pos.y - y), 0))
+                x = int(round(pos.x + a*(pos.y - y), 0))
                 while x < self.dimension:
-                    if not self.notwallmap[y][x]:
+                    if not self.wallmap[y][x]:
                         tiles_to_see.append(Pos(x, y))
                     x += 1
-        elif self.direction == Direction.LEFT:
+        elif direction == Direction.LEFT:
             for y in range(self.dimension):
-                if y == self.pos.y:
+                if y == pos.y:
                     a = -a
-                x = int(round(self.pos.x + a*(y - self.pos.y), 0))
+                x = int(round(pos.x + a*(y - pos.y), 0))
                 while x >= 0:
-                    if not self.notwallmap[y][x]:
+                    if not self.wallmap[y][x]:
                         tiles_to_see.append(Pos(x, y))
                     x -= 1
         return tiles_to_see
+
+    def get_tiles_to_see(self) -> list[Pos]: # old one
+        return self.get_tiles_probably_visible(self.pos, self.direction, self.turretDirection)
 
     def update_diretion(self, tile: Tile):
         for entity in tile.entities:
@@ -109,6 +118,18 @@ class MyBot(HackathonBot):
                 self.direction = entity.direction
                 self.turretDirection = entity.turret.direction
                 break
+
+    def update_items_list(self, tiles: list[list[Tile]], visible_tiles: list[Pos], tick: int):
+        for pos in visible_tiles:
+            tile = tiles[pos.y][pos.x]
+            for entity in tile.entities:
+                if isinstance(entity, Item):
+                    self.knownItems[pos] = ItemData(entity.type, tick)
+        for pos, item in self.knownItems.items():
+            item: ItemData
+            if tick - item.tick > 100:
+                self.knownItems.pop(pos)
+                    
 
     def on_lobby_data_received(self, lobby_data: LobbyData) -> None:
         self.dimension = lobby_data.server_settings.grid_dimension
@@ -118,6 +139,7 @@ class MyBot(HackathonBot):
         if not self.init:
             self.analize_map(game_state.map)
         self.update_diretion(game_state.map.tiles[self.pos.y][self.pos.x])
+        self.update_items_list(game_state.map.tiles, self.get_tiles_to_see(), game_state.tick)
         if game_state.tick % 10 == 0:
             tiles_to_see = self.get_tiles_to_see()
             print([(pos.x, pos.y) for pos in tiles_to_see])
